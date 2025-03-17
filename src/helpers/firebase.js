49b07e6b49b90cc8dc740d455a9f62f8d4549e14
JuliGarc91 <julissagarcia@pursuit.org>
2025-03-16 20:27:39 -0400

    added supabase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+// import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,9 +25,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 //const analytics = getAnalytics(app);
-const storage = getStorage(app);
+// const storage = getStorage(app);
 
-export { storage, ref, getDownloadURL };
+// export { storage, ref, getDownloadURL };
 export const auth = getAuth()
 export const db = getFirestore(app)
 export default app
